a41839af526179090e0e8b2268db87fbad69b4df
oneraul <oneraul@gmail.com>
2020-03-16 13:11:29 +0100

    Parsing the json file and outputting shader configs to the console.
    Former-commit-id: b1c3569f9a6f350bbad784754a22d5151a91d31b

--- ShaderWatch.cpp
+++ ShaderWatch.cpp
@@ -1,5 +1,45 @@
-#include "shaderc.hpp"
+//#include "shaderc.hpp"
+#include "json.hpp"
+#include <iostream>
+#include <fstream>
+
+using json = nlohmann::json;
+
+void print(json& element, std::string suffix)
+{
+	if (element.is_string())
+	{
+		std::string name = element;
+		std::cout << name << "." << suffix << " -o " << name << "_" << suffix << ".spv" << "\n";
+	}
+	else
+	{
+		std::string file = element["file"];
+		for (auto& variant : element["variants"])
+		{
+			std::cout << file << "." << suffix;
+			for (auto& define : variant["defines"])
+				std::cout << " -D" << static_cast<std::string>(define);
+			std::cout << " -o " << static_cast<std::string>(variant["outputFile"]) << "\n";
+		}
+	}
+}
 
 int main()
 {
+	std::ifstream i("shaders.json");
+	json j;
+	i >> j;
+
+	std::cout << " Vertex shaders: ------------------------------------------\n";
+	for (auto& element : j["vertex"])
+		print(element, "vert");
+
+	std::cout << "\n Fragment shaders: ----------------------------------------\n";
+	for (auto& element : j["fragment"])
+		print(element, "frag");
+
+	std::cout << "\n Compute shaders: -----------------------------------------\n";
+	for (auto& element : j["compute"])
+		print(element, "comp");
 }
