07ce3f2bbac19dfd6d065b488aa6f1fd10d03067
oneraul <oneraul@gmail.com>
2020-03-20 09:13:52 +0100

    Added watch loop.
    Former-commit-id: 52cfd3f3c0bc2e1a22bdb5fdf37aef0c02a40304

--- ShaderWatch.cpp
+++ ShaderWatch.cpp
@@ -1,25 +1,39 @@
 #include <fstream>
+#include <thread>
 #include "Shader.h"
 
 int main()
 {
-	ColoredConsole console;
+	rmkl::ColoredConsole console;
+	shaderc::Compiler compiler;
+	std::vector<rmkl::Shader> shaders;
 
-	std::ifstream i("shaders.json");
-	nlohmann::json j;
-	i >> j;
-
-	std::vector<Shader> shaders;
-
-	for (auto& element : j["vertex"])
-		shaders.emplace_back(element, "vert");
-	for (auto& element : j["fragment"])
-		shaders.emplace_back(element, "frag");
-	for (auto& element : j["compute"])
-		shaders.emplace_back(element, "comp");
-
-	for (auto& shader : shaders)
+	console.print("Initializing ShaderWatch.\n\n", rmkl::ColoredConsole::Color::White);
 	{
-		shader.compile(console);
+		std::ifstream i("shaders.json");
+		nlohmann::json j;
+		i >> j;
+
+		for (auto& element : j["vertex"])
+			shaders.emplace_back(element, rmkl::Shader::Type::Vertex);
+		for (auto& element : j["fragment"])
+			shaders.emplace_back(element, rmkl::Shader::Type::Fragment);
+		for (auto& element : j["compute"])
+			shaders.emplace_back(element, rmkl::Shader::Type::Compute);
+
+		for (auto& shader : shaders)
+		{
+			shader.compile(compiler, console, true);
+		}
+	}
+
+	console.print("\nWatch started.\n\n", rmkl::ColoredConsole::Color::White);
+	while (true)
+	{
+		for (auto& shader : shaders)
+		{
+			shader.compile(compiler, console);
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 }
